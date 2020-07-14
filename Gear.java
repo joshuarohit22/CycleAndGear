56ea4bb3c5d68f84d1dc86f20032c949d090344d
joshuarohit22 <joshuarohit22@gmail.com>
2020-07-14 23:40:58 +0530

    added gearInches to Gear class

--- Gear.java
+++ Gear.java
@@ -5,13 +5,23 @@ public class Gear {
 	private int chainring;
 	private int cog;
 
-	public Gear(int chainring, int cog) {
+	private int rim;
+	private double tire;
+
+	public Gear(int chainring, int cog, int rim, double tire) {
 		this.chainring = chainring;
 		this.cog = cog;
+		this.rim = rim;
+		this.tire = tire;
 	}
 
 	public double ratio() {
 		return (chainring * 1.0) / (cog * 1.0);
 	}
 
+	public double gearInches() {
+		return this.ratio() * ( rim + (tire * 2) );
+	}
+
 }
+
